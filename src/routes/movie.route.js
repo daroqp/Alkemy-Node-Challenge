e3c79dff3e4dc37e1335a47ce564f23272064bef
Dario Quispe <daro.qp@gmail.com>
2022-04-09 00:08:05 -0300

    feat: Auth middleware

--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -1,17 +1,18 @@
 const { Router } = require('express');
 const { getMovies, movieDetail, postMovie, editMovie, deleteMovie } = require('../controllers/movie.controller');
 const { validateMovie, validateIdMovie, validateFilterMovie } = require('../middlewares/validators/movies.validator')
+const { checkAuth, checkRoleAuth } = require('../middlewares/auth.middleware') 
 const uploadFile = require('../middlewares/fileUpload.middleware');
 const router = Router();
 
-router.get('/', validateFilterMovie , getMovies );
+router.get('/', checkAuth , checkRoleAuth(['admin', 'user']) , validateFilterMovie , getMovies );
 
-router.get('/:movie_id', validateIdMovie , movieDetail );
+router.get('/:movie_id', checkAuth , checkRoleAuth(['admin', 'user']) , validateIdMovie , movieDetail );
 
-router.post('/create', uploadFile , validateMovie , postMovie );
+router.post('/create', checkAuth , checkRoleAuth(['admin']) , uploadFile , validateMovie , postMovie );
 
-router.put('/:movie_id', validateIdMovie , validateMovie , editMovie );
+router.put('/:movie_id', checkAuth , checkRoleAuth(['admin']) , validateIdMovie , validateMovie , editMovie );
 
-router.delete('/:movie_id', validateIdMovie , deleteMovie );
+router.delete('/:movie_id', checkAuth , checkRoleAuth(['admin']) , validateIdMovie , deleteMovie );
 
 module.exports = router;
